c8bc8566e09c21e0ac86c0319094379326b65792
Bruce Christie <bqchristie@gmail.com>
2022-07-25 12:58:59 -0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,82 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>
+      TVO Weather
+    </h1>
+    <input type="text" placeholder="Enter city...." @keyup="refreshCities" v-model="query"><input
+      type="button" @click="clearQuery" value="Clear">
+    <ul class="results">
+      <li v-for="(city, idx) in cities" :key="'part-' + idx" @click="setCity(city)">{{ city.name }}, {{ city.state }},
+        {{ city.country }}
+      </li>
+    </ul>
+    <div v-if="selectedCity">
+      <h2>Current Weather for {{ selectedCity?.name }} {{ selectedCity?.state }} {{ selectedCity?.country }}</h2>
+      <WeatherSummary :weather="weather"/>
+    </div>
+
+    <div v-if="!selectedCity">
+      <h2>Select a city from the text box above</h2>
+    </div>
+
+<!--    <div>-->
+<!--      <pre><code>{{weather}}</code></pre>-->
+<!--    </div>-->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import WeatherSummary from "@/WeatherSummary";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {WeatherSummary},
+  data() {
+    return {
+      cities: [],
+      selectedCity: null,
+      weather: null,
+      units: 'metric',
+      query: null,
+    }
+  },
+  methods: {
+
+    async refreshCities() {
+      console.log("refreshing cities")
+      console.log(this.query)
+
+      const cityData = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${this.query}&limit=5&appid=bf89e941da14d9a5e0a60764ed99bf4a`)
+      this.cities = await cityData.json()
+      console.log(cityData)
+    },
+
+    async refreshWeatherData() {
+      const url =
+          `https://api.openweathermap.org/data/2.5/weather?lat=${this.selectedCity.lat}&lon=${this.selectedCity.lon}&appid=bf89e941da14d9a5e0a60764ed99bf4a&units=${this.units}`
+      const weatherData = await fetch(url)
+      this.weather = await weatherData.json()
+      console.log(url)
+    },
+
+    setCity(city) {
+      this.selectedCity = city;
+      this.cities = []
+    },
+
+    clearQuery() {
+      this.query = null
+      this.setCity(null)
+    }
+
+
+  },
+  watch: {
+    selectedCity(newval) {
+      newval ? this.refreshWeatherData() : this.weather = null
+    }
+  },
 }
 </script>
 
@@ -24,5 +88,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+input {
+  height: 1.8em;
+  font-size: 1.6em;
 }
 </style>
---
+++ src/WeatherSummary.vue
@@ -0,0 +1,104 @@
+<template>
+  <div id="weather-summary">
+    <div class="lug">
+      <h2>Conditons</h2>
+      <img :src="iconSource">
+    </div>
+    <div class="lug">
+      <h2>Current Temp</h2>
+      <h1>
+      {{ weather?.main?.temp }}
+      </h1>
+      <div class="secondary">
+        <div>
+          <h2>Min</h2>
+          {{ weather?.main?.temp_min }}
+        </div>
+        <div>
+          <h2>Max</h2>
+          {{ weather?.main?.temp_max }}
+        </div>
+      </div>
+    </div>
+    <div class="lug">
+      <h2>Humidity</h2>
+      <h1>{{weather?.main?.humidity}}%</h1>
+    </div>
+    <div class="lug">
+      <h2>Sunrise</h2>
+      <h1>{{weather?.main?.humidity}}</h1>
+      <h2>Sunset</h2>
+      <h1>{{weather?.main?.humidity}}</h1>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'WeatherSummary',
+  props: {
+    weather: {}
+  },
+  computed: {
+    iconSource() {
+      return `http://openweathermap.org/img/wn/${this.weather?.weather[0].icon}@2x.png" alt="${this.weather?.weather[0].description}`
+    }
+  },
+}
+</script>
+<style scoped>
+
+#weather-summary {
+  /*height: 6em;*/
+  display: flex;
+  justify-content: space-between;
+}
+
+@media only screen and (max-width: 768px) {
+  #weather-summary {
+    flex-direction: column;
+  }
+}
+
+h1 {
+  margin-top: 0;
+  margin-bottom: 0;
+  text-align: left;
+}
+
+h2 {
+  margin-top: 0;
+  margin-bottom: 0;
+  font-size: 0.5em;
+  text-align: left;
+}
+
+h4 {
+  text-align: left;
+  width: 100%;
+}
+
+.lug {
+  border-radius: 0.5em;
+  padding-left: 10px;
+  padding-right: 10px;
+  padding-top: 10px;
+  background-color: #559eeb;
+  color: #fff;
+  width:100px;
+  margin-right:1em;
+  height: 8em;
+}
+
+@media only screen and (max-width: 768px) {
+  .lug {
+    margin-bottom: 0.8em;
+    width: calc(100vw - 2.4em);
+  }
+}
+
+.lug .secondary {
+  display: flex;
+  justify-content: space-between;
+}
+
+</style>
